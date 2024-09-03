c71cfd74a6f44aee6d7fa5615d482ac196321e7a
Pedro Reginaldo Tome Silva <67759198+PedroSilva201@users.noreply.github.com>
2024-09-03 16:13:09 -0300

    Create atividade1.c

---
+++ lista_1/atividade1.c
@@ -0,0 +1,16 @@
+/**⦁	Dado dois números inteiros positivos n, calcular e imprimir a sua soma.**/
+
+#include <stdio.h>
+int main(){
+    int X;
+    int A;
+    int B;
+    printf("Digite o valor A: ");
+    scanf("%d",&A);
+    printf("Digite o valor B: ");
+    scanf("%d",&B);
+    X = A + B;
+    printf("----------------");
+    printf("\nsoma total:%d \n",X);
+    return 0;
+}
