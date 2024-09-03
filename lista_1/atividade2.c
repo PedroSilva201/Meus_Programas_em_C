91e6d078861cfe190a0d4fea18247357be74463c
Pedro Reginaldo Tome Silva <67759198+PedroSilva201@users.noreply.github.com>
2024-09-03 16:26:59 -0300

    Delete lista_1/atividade2.c

--- lista_1/atividade2.c
+++
@@ -1,9 +0,0 @@
-/**⦁	Faça um programa que solicite um número (inteiro/positivo) e imprima-o.**/
-
-#include <stdio.h>
-int main(){
-    int numero;
-    printf("Digite um numero inteiro/positivo: ");
-    scanf("%d",&numero);
-    return 0;
-}
