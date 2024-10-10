c7a4a528e02f2723b5fc75ad367fd40d12af28b2
Pedro Reginaldo Tome Silva <67759198+PedroSilva201@users.noreply.github.com>
2024-10-10 10:43:47 -0300

    Create atividade_03.C

---
+++ lista_6/atividade_03.C
@@ -0,0 +1,9 @@
+/*Seja o seguinte trecho de programa:
+int i=3, j=5;
+int *p, *q;
+p = &i;
+q = &j;
+
+Qual é o valor da variável  resultado do tipo int?
+a) resultado=p == &i;     	b)resultado= *p - *q   c) resultado=(12 + *p)/(*q)+7; 
+*/
