7f33189743aa49e1fad517d1436fd32a3ea75fcb
Pedro Reginaldo Tome Silva <67759198+PedroSilva201@users.noreply.github.com>
2024-10-10 10:56:11 -0300

    Create atividade09.c

---
+++ lista_6/atividade09.c
@@ -0,0 +1,4 @@
+/*Crie a função chamada Concatena (char *str_destino, char *str_origem) usando ponteiros.
+Regras: - Concatena duas strings. A string de origem permanecerá inalterada e será anexada ao fim da string de destino.
+Obs: não é permitido utilizar strcat.  (Faça o principal também-> O usuário deverá digitar duas strings que serão passadas por parâmetros para função concatena).
+*/
